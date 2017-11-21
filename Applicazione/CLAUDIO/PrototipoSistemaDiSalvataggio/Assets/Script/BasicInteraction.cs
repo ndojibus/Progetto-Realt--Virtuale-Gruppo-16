c99e0414ceccddb789e7d69d6ae9a630083985df
Unknown <claudegia94@libero.it>
2017-11-21 23:02:39 +0100

    Added a Unity Project for testing the Save System
    Nella cartella Applicazione/CLAUDIO ho aggiunto il prototipo per testare il sistema di salvataggio (pensa contenga anche codice utile riciclabile per interagire con un oggetto)

---
+++ Applicazione/CLAUDIO/PrototipoSistemaDiSalvataggio/Assets/Script/BasicInteraction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicInteraction : PersistentData
+{
+    List<GameObject> m_childs;
+    CanvasGroup m_actionCanvas;
+    int m_activeIndex = -1;         //persistent data, index 0
+    bool m_activated = false;
+
+	// Use this for initialization
+	void Start () {
+        m_childs = new List<GameObject>();
+        foreach (Transform child in transform)
+            m_childs.Add(child.gameObject);
+        if (m_childs.Count == 0)
+            Debug.LogError("Impossible to find chidren!");
+        m_actionCanvas = GameObject.Find("ActionCanvas").GetComponent<CanvasGroup>();
+        if (m_actionCanvas == null)
+            Debug.LogError("Impossible to find ActionCanvas!");
+
+        createData((UInt64)m_activeIndex);      //so index is 0
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other) {
+        if ((other.tag == "Player") && (Vector3.Angle(other.transform.forward, this.transform.position - other.transform.position) < 90f))
+        {
+            m_actionCanvas.alpha = 1;
+            m_activated = true;
+        }
+        else
+        {
+            m_actionCanvas.alpha = 0;
+            m_activated = false;
+        }
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1") && m_activated)
+        {
+            if (m_activeIndex < 0)
+            {
+                m_activeIndex = 0;
+                m_childs[m_activeIndex].SetActive(true);
+            }
+            else
+            {
+                m_childs[m_activeIndex++].SetActive(false);
+                if (m_activeIndex >= m_childs.Count)
+                    m_activeIndex = -1;
+                else
+                    m_childs[m_activeIndex].SetActive(true);
+            }
+            saveData(0, (UInt64)m_activeIndex);
+            SceneControl.sceneControl.Save();
+        }
+    }
+
+    public override bool loadData(int t_key, UInt64 t_data) {
+        bool find = base.loadData(t_key, t_data);
+        if (((int)t_data >= 0))
+        {
+            m_activeIndex = (int)t_data;
+            m_childs[m_activeIndex].SetActive(true);
+        }
+        return find;
+    }
+}
---
+++ Applicazione/CLAUDIO/PrototipoSistemaDiSalvataggio/Assets/Script/SceneControl.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+class AscendingComparer<TKey> : IComparer<int>
+{
+
+    public int Compare(int x, int y)
+    {
+        return x.CompareTo(y);
+    }
+}
+
+public class SceneControl : MonoBehaviour
+{
+    public static SceneControl sceneControl;
+    SortedList<int, PersistentData> m_persistentObjectList;
+    SortedList<int, UInt64> m_persistentDataList;
+    int m_currentSceneIndex;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (sceneControl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            sceneControl = this;
+            m_currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            m_persistentObjectList = new SortedList<int, PersistentData>(new AscendingComparer<int>());
+            m_persistentDataList = new SortedList<int, UInt64>(new AscendingComparer<int>());
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Reload")) {
+
+            SceneManager.LoadScene(m_currentSceneIndex);
+        }
+            
+    }
+
+    public void Save() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Create);
+
+        m_persistentDataList.Clear();
+        foreach (KeyValuePair<int, PersistentData> persistent in m_persistentObjectList) {
+            foreach (Data data in persistent.Value.objectData) {
+                m_persistentDataList.Add(data.key, data.data);
+            }
+        }
+
+        bf.Serialize(file, m_persistentDataList);
+        file.Close();
+    }
+
+    public void Load() {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            m_persistentDataList.Clear();
+            m_persistentDataList = (SortedList<int, UInt64>)bf.Deserialize(file);
+
+            int i = 0;
+            //modifica i valori corrispondenti dei vari gameobject
+            foreach (KeyValuePair<int, PersistentData> persistent in m_persistentObjectList) {
+
+                //1000 index per ogni scena di cui 10 dedicati ad oggetto
+                int minimumKey = 1000 * m_currentSceneIndex + 10 * persistent.Key;
+                int maximumKey = minimumKey + 10;
+
+                for (; 
+                    (i < m_persistentDataList.Count) && 
+                    ( 
+                        (m_persistentDataList.Keys[i] >= minimumKey) && 
+                        (m_persistentDataList.Keys[i] < maximumKey)
+                    ); 
+                    i++) {
+
+
+                    if (!persistent.Value.loadData(m_persistentDataList.Keys[i], m_persistentDataList.Values[i]))
+                        Debug.LogError("Impossible to load values of key: " + m_persistentDataList.Keys[i]);
+                }
+            }
+
+            file.Close();
+        }
+        else
+            Debug.LogError("Impossible to load save data");
+    }
+
+    //scene loading
+    void OnEnable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        PersistentData.objectCount = 0;
+
+        m_persistentObjectList.Clear();
+        PersistentData[] persistentList = FindObjectsOfType<PersistentData>();
+        foreach (PersistentData persistent in persistentList)
+        {
+            m_persistentObjectList.Add(persistent.objectKey, persistent);
+        }
+
+        Invoke("Load", 1);
+    }
+}
