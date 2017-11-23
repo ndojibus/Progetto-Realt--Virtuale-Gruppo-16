e2e5096141ba45dec8259859d7ffba6304c8e2e5
Unknown <claudegia94@libero.it>
2017-11-23 21:50:54 +0100

    Added a Unity Project for testing the Laser System
    Nella cartella Applicazione/CLAUDIO ho aggiunto il prototipo per testare i laser

---
+++ Applicazione/CLAUDIO/PrototipoLaser/Assets/Scripts/LaserBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Classe che determina il comportamento del laser
+ * 
+ * DA MIGLIORARE: come rappresentare visivamente il laser, per adesso è un cilindro con materiale emettente
+ */
+public class LaserBehaviour : MonoBehaviour {
+    [SerializeField]
+    float m_maximumRayLenght = 10f;             //IMPOSTABILE VIA EDITOR: lunghezza massima del raggio
+
+    [SerializeField]
+    float m_angularVelocity = 90f;              //IMPOSTABILE VIA EDITOR: velocità angolare del laser (di quanti gradi si sposta in un secondo)
+
+    float m_rayLenght;                          //lunghezza attuale del laser
+    GameObject m_laser;                         //gameobject che rappresenta un cilindro con materiale emettente per fare il laser, usato per prova
+
+	// Inizializza l'oggetto laser prendendo il primo figlio (se ne aggiungi altri ricorda questo dettaglio)
+	void Awake () {
+        m_laser = transform.GetChild(0).gameObject;
+        if (m_laser == null)
+            Debug.LogError("Impossible to find laser!");
+    }
+	
+	// Nell'update chiamo 3 funzioni, controlla quello che fanno nella loro dichiarazione sotto
+	void Update () {
+        InputManager();
+        RayCast();
+        ChangeLaser();
+    }
+
+    // Funzione usata per disegnare il gizmo della lunghezza attuale reale del raggio laser, per vederlo clicca "Gizmos" in alto a destra dalla finestra del gioco
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(this.transform.position, this.transform.forward * m_rayLenght);
+    }
+
+    /* Funzione che gestisce la rotazione del laser in base alla pressione delle frecce destra-sinistra o i tasti a-d
+     * 
+     *  transform.RotateAround ruota un il laser rispetto al vettore up del gameobject di un angolo dato dalla velocità angolare * il tempo impiegato dall'ultimo frame
+     */
+    private void InputManager() {
+        if (Input.GetButton("Horizontal"))
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+                this.transform.RotateAround(this.transform.position, this.transform.up, -m_angularVelocity * Time.deltaTime);
+
+            if (Input.GetAxis("Horizontal") > 0)
+                this.transform.RotateAround(this.transform.position, this.transform.up, m_angularVelocity * Time.deltaTime);
+        }
+    }
+
+    // Emette il raggio e se incontra un interruttore lo attiva
+    private void RayCast() {
+        RaycastHit hit;     //contiene informazioni sulla collisione del raggio
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit))  //se raggio che va dalla posizione dell'oggetto verso avanti colpisce qualcosa
+        {
+            m_rayLenght = Vector3.Distance(this.transform.position, hit.point);         //imposta la lunghezza del raggio come la distanza fra l'oggetto e il punto di collisione    
+            if (hit.collider.tag == "Activator")                                        //se l'oggetto colpito dal raggio ha la tag Activator
+                hit.collider.gameObject.SendMessageUpwards("Activate");                 //instanzia la funzione Activate sull'interruttore
+        }
+        else
+            m_rayLenght = m_maximumRayLenght;                                           //se no imposta la lunghezza del raggio a quella massima
+    }
+
+    /* Funzione che cambia l'aspetto del laser visualizzato
+     * 
+     * UN APPUNTO: il cilindro standard di unity è 2x2 ed ha il pivot nel punto centrale e non alla base,
+     * per questo oltre a scalarlo di una dimensione pari alla lunghezza del raggio/2, e non solo la lunghezza del raggio,
+     * va anche traslato in avanti di newPosition, ovvero metà della lunghezza del raggio
+     */
+    private void ChangeLaser()
+    {
+        Vector3 newPosition = new Vector3(0, 0, m_rayLenght/2);
+        Vector3 newScale = new Vector3(0.05f, m_rayLenght/2, 0.05f);
+        m_laser.transform.localPosition = newPosition;
+        m_laser.transform.localScale = newScale;
+    }
+}
---
+++ Applicazione/CLAUDIO/PrototipoLaser/Assets/Scripts/TriggerActionBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Ho creato questa classe astratta nel caso dovessimo avere più comportamenti attivati dal laser,
+ * devi semplicemente far ereditare da questa classe lo script che causerà l'azione attivata dal laser
+ */
+public abstract class TriggerActionBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public abstract void Activate();
+}
