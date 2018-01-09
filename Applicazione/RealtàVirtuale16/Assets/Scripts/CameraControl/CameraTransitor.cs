0937d3d7f2051d6a88db4fcf60499ea1dae53110
Unknown <claudegia94@libero.it>
2018-01-09 19:29:28 +0100

    Aggiunto ultima versione progetto

---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/CameraControl/CameraTransitor.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct CameraTransition {
+    public GameObject transitionPoint;
+    public float transitionSpeed;
+}
+
+public class CameraTransitor : MonoBehaviour {
+
+    [SerializeField]
+    private int m_transitorID;
+
+    [SerializeField]
+    private List<CameraTransition> m_transitionList;
+
+    [SerializeField]
+    private bool m_forward = true;
+
+    [SerializeField]
+    private bool m_moving = true;
+
+    private float m_currentTransitionPoint = 0;
+    private int m_currentTransitionIndex = 0;
+
+    private bool m_initialized = false;
+    
+    public bool moving
+    {
+        get { return m_moving; }
+        set { m_moving = value; }
+    }
+
+    public bool forward
+    {
+        get { return m_forward; }
+        set { m_forward = value; }
+    }
+
+    public int transitorID {
+        get { return m_transitorID;  }
+        set { m_transitorID = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (m_transitionList != null && m_transitionList.Count >= 2)
+            m_initialized = true;
+        else
+            Debug.LogError("transitionList should have at least 2 elements!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_initialized && m_moving)
+        {
+            if ((m_currentTransitionPoint >= 1f) && m_forward)
+            {
+                m_currentTransitionIndex++;
+                m_currentTransitionPoint = 0f;
+
+            } else if ((m_currentTransitionPoint <= 0f) && !m_forward)
+            {
+                m_currentTransitionIndex--;
+                m_currentTransitionPoint = 1f;
+            }
+
+
+
+            if ((m_currentTransitionIndex + 1 < m_transitionList.Count) && (m_currentTransitionIndex >= 0))
+            {
+
+                var currentTransition = m_transitionList[m_currentTransitionIndex];
+                var nextTransition = m_transitionList[m_currentTransitionIndex + 1];
+
+                if (m_forward)
+                    m_currentTransitionPoint += Time.deltaTime * currentTransition.transitionSpeed;
+                else
+                    m_currentTransitionPoint -= Time.deltaTime * nextTransition.transitionSpeed;
+
+                m_currentTransitionPoint = Mathf.Clamp(m_currentTransitionPoint, 0f, 1f);
+
+                transform.position = Vector3.Lerp(currentTransition.transitionPoint.transform.position,
+                                        nextTransition.transitionPoint.transform.position,
+                                        m_currentTransitionPoint);
+
+                transform.localRotation = Quaternion.Lerp(currentTransition.transitionPoint.transform.rotation,
+                                                    nextTransition.transitionPoint.transform.rotation,
+                                                    m_currentTransitionPoint);
+            }
+            else if (m_currentTransitionIndex + 1 >= m_transitionList.Count)
+            {
+                var currentTransition = m_transitionList[m_transitionList.Count - 2];
+                var nextTransition = m_transitionList[m_transitionList.Count - 1];
+
+                transform.position = Vector3.Lerp(currentTransition.transitionPoint.transform.position,
+                                        nextTransition.transitionPoint.transform.position,
+                                        1f);
+
+                transform.localRotation = Quaternion.Lerp(currentTransition.transitionPoint.transform.rotation,
+                                                    nextTransition.transitionPoint.transform.rotation,
+                                                    1f);
+            }
+            else if(m_currentTransitionIndex < 0)
+            {
+                    var currentTransition = m_transitionList[0];
+                    var nextTransition = m_transitionList[1];
+
+                    transform.position = Vector3.Lerp(currentTransition.transitionPoint.transform.position,
+                                            nextTransition.transitionPoint.transform.position,
+                                            0f);
+
+                    transform.localRotation = Quaternion.Lerp(currentTransition.transitionPoint.transform.rotation,
+                                                        nextTransition.transitionPoint.transform.rotation,
+                                                        0f);
+            }
+
+
+        }
+    }
+}
---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/Saving/InteractablePickup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePickup : InteractableObject_Abstract
+{
+    
+    GameObject m_item;
+
+    // Use this for initialization
+    void Start () {
+        base.Start();
+
+        m_descriptionText.text = m_description;
+
+        m_item = this.transform.Find("Rubino").gameObject;
+        
+
+        if (m_item == null)
+            Debug.LogError("Select an item!");
+
+        m_equiped = true;
+
+        createData(1);  //index 0, 1 significa che il rubino è ancora incastonato
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        base.Update();
+
+        if (m_equiped != m_item.activeSelf)
+            m_item.SetActive(m_equiped);
+
+
+        if (m_inspectMode && m_equiped && Input.GetKeyDown(KeyCode.E) && !m_camerasInTransition)
+        {
+            
+            TakeRuby();
+            SetAction();
+
+        }
+
+	}
+
+    private void TakeRuby()
+    {
+
+        //inserito nell'inventario
+        m_inventory.PickRuby();
+
+        //cancellato dalla scena
+
+        m_item.SetActive(false);
+        m_equiped = false;
+
+        saveData(0, 0);   // salva che il rubino è stato preso
+    }
+
+    protected override void SetAction()
+    {
+        if (!m_inspectMode)
+        {
+            m_inspectText.text = "Premi E per Esaminare";
+            toggleInspectPanel(true);
+        }
+        else if (m_inspectMode && m_equiped)
+        {
+            m_inspectText.text = "Premi E per Raccogliere il Rubino";
+            toggleInspectPanel(true);
+
+        }
+        else
+        {
+            toggleInspectPanel(false);
+
+        }
+    }
+
+    
+    protected override void objectControl() {
+    }
+    protected override void timerEndActions()
+    {
+    }
+
+    
+
+    protected override void EndingClickActions() {
+    }
+
+    public override bool loadData(int t_key, ulong t_data)
+    {
+        bool find = base.loadData(t_key, t_data);
+        if (t_data == 0 && find)
+        {
+            m_equiped = false;
+            
+        }
+        return find;
+    }
+}
