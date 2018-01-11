6e6e1ab5b06b16e82148602628e5c372737747ef
ndojibus <33726083+ndojibus@users.noreply.github.com>
2018-01-11 21:21:21 +0100

    Update con il nuovo funzionamento delle porte by Claudio
    spero di aver fatto tutto bene

--- Applicazione/RealtàVirtuale16/Assets/Scripts/Laser/RayActivator.cs
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/Laser/RayActivator.cs
@@ -17,41 +17,46 @@ public class RayActivator : TriggerActionBase
 
     float m_actualTimer;            //timer decrescente
     bool m_activated = false;       //interruttore attivo/disattivo
-    Animator m_animatorComponent;   //animatore della porta
+    CameraTransitor m_transitorComponent;   //animatore della porta
 
     // inizializza il timer e l'animatore collegato alla porta, ricorda di impostare qual è la porta via editor
-    public void Awake() {
-        m_actualTimer = m_disableTime; 
-        if (m_doorObject != null) {          
-            m_animatorComponent = m_doorObject.GetComponent<Animator>();
-            if (m_animatorComponent == null)
+    public void Awake()
+    {
+        m_actualTimer = m_disableTime;
+        if (m_doorObject != null)
+        {
+            m_transitorComponent = m_doorObject.GetComponentInChildren<CameraTransitor>();
+            if (m_transitorComponent == null)
                 Debug.LogError("Impossible to find animator!");
         }
         else
             Debug.LogError("Impossible to find door object!");
     }
-	
-	// Update is called once per frame
-	public void Update () {
 
-        if (m_activated) {                                      //se l'interruttore è attivo
-            m_actualTimer -= Time.deltaTime;                    //decrementa il timer con il tempo passato nell'ultimo frame 
-            if (m_actualTimer <= 0f) {                          //se il timer è scaduto
-                m_activated = false;                            //disattiva l'interruttore
-                m_actualTimer = m_disableTime;                  //resetta il timer
-                m_animatorComponent.Play("DoorClosing");        //attiva l'animazione che chiude la porta
+    // Update is called once per frame
+    public void Update()
+    {
+
+        if (m_activated)
+        {                                                          //se l'interruttore è attivo
+            m_actualTimer -= Time.deltaTime;                                        //decrementa il timer con il tempo passato nell'ultimo frame 
+            if (m_actualTimer <= 0f)
+            {                                              //se il timer è scaduto
+                m_activated = false;                                                //disattiva l'interruttore
+                m_actualTimer = m_disableTime;                                      //resetta il timer
+                m_transitorComponent.forward = !m_transitorComponent.forward;       //attiva l'animazione che chiude la porta
             }
         }
     }
 
     public override void Activate()
     {
-        if (!m_activated)                                       //se l'interruttore non è attivo
+        if (!m_activated)                                                               //se l'interruttore non è attivo
         {
-            m_activated = true;                                 //lo attivi         
-            m_animatorComponent.Play("DoorOpening");            //attiva l'animazione che apre la porta
+            m_activated = true;                                                         //lo attivi         
+            m_transitorComponent.forward = !m_transitorComponent.forward;               //attiva l'animazione che apre la porta
         }
 
-        m_actualTimer = m_disableTime;                          //in ogni caso resetta il timer
+        m_actualTimer = m_disableTime;                                                  //in ogni caso resetta il timer
     }
 }
