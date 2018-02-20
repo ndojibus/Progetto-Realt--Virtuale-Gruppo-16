74d0a46fae7a4707fc2cb3ff3b47c725871fa755
Unknown <ndojo91@gmail.com>
2018-02-20 23:41:32 +0100

    Aggiunti spuntoni della morte

---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/TriggerMorto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMorto : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("MORTOOOOO");
+        }
+    }
+}
