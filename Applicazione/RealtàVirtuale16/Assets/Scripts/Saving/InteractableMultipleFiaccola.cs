63427857bbbfe32383908ca257a7322609b4fefc
Unknown <claudegia94@libero.it>
2018-02-22 00:13:50 +0100

    Ho creato la mia metà della Scena2

---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/Saving/InteractableMultipleFiaccola.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableMultipleFiaccola : InteractableObject_Abstract
+{
+    [SerializeField]
+    CameraTransitor m_doorTransitor;
+
+    [SerializeField]
+    InteractableMultipleFiaccola m_otherFiaccola;
+
+    GameObject m_fiaccola;
+    CameraTransitor m_fiaccolaTransitor;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        base.Start();
+
+        if (m_doorTransitor == null)
+            Debug.LogError(this.name + ": Impossible to find door transitor");
+
+        m_fiaccola = this.transform.Find("Fiaccola").gameObject;
+        if (m_fiaccola != null)
+        {
+            m_fiaccolaTransitor = m_fiaccola.GetComponent<CameraTransitor>();
+            if (m_fiaccolaTransitor == null)
+                Debug.LogError(this.name + ": Impossible to find fiaccola transitor");
+        }
+        else
+            Debug.LogError(this.name + ": Impossible to find fiaccola");
+        m_equiped = false;
+
+        createData(0);  //index 0, 0 significa che l'interruttore non è ancora attivato
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        base.Update();
+
+        if (m_equiped && m_fiaccolaTransitor.forward != true)
+            m_fiaccolaTransitor.forward = true;
+            
+
+
+        if (m_inspectMode && !m_equiped && Input.GetKeyDown(KeyCode.E) && !m_camerasInTransition)
+        {
+            m_equiped = true;
+            UpdateUI();
+        }
+
+    }
+
+    protected override void TransitionOutActions()
+    {
+
+        base.TransitionOutActions();
+
+        if(m_equiped && m_otherFiaccola.equiped && m_doorTransitor.forward != true)
+            m_doorTransitor.forward = true;
+
+    }
+
+    protected override void UpdateUI()
+    {
+
+
+
+        // ***NON INSPECT MODE***
+        if (!m_inspectMode)
+        {
+
+            m_actionText = "Premi E per Esaminare";
+
+        }
+
+        base.UpdateUI();
+
+        // *** INSPECT MODE ***
+        if (!m_camerasInTransition)
+        {
+
+
+
+            if (m_inspectMode)
+            {
+                if (!m_equiped)
+                    m_uiManager.ToggleActionPanel(true, "Premi E per Tirare");
+                else
+                    m_uiManager.ToggleActionPanel(false);
+            }
+
+        }
+
+
+    }
+
+    /*IEnumerator ApriChiudiCoperchio(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = m_coperchio.transform.rotation;
+        var toAngle = Quaternion.Euler(m_coperchio.transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            m_coperchio.transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }*/
+
+
+    public override bool loadData(int t_key, ulong t_data)
+    {
+        bool find = base.loadData(t_key, t_data);
+        if (t_data == 1 && find)
+        {
+            m_equiped = true;
+
+        }
+        return find;
+    }
+}
+
---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/Saving/InteractablePickToggle.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePickToggle : InteractableObject_Abstract
+{
+
+    [SerializeField]
+    CameraTransitor m_doorTransitor;
+
+    GameObject m_pickup1;
+    GameObject m_pickup2;
+    // Use this for initialization
+    void Start () {
+        base.Start();
+
+        if (m_doorTransitor == null)
+            Debug.LogError(this.name + ": No door transitor selected");
+
+        m_pickup1 = this.transform.Find("Pickup1").gameObject;
+        if (m_pickup1 == null)
+            Debug.LogError(this.name + ": There is no pickup1!");
+
+        m_pickup2 = this.transform.Find("Pickup2").gameObject;
+        if (m_pickup2 == null)
+            Debug.LogError(this.name + ": There is no pickup2!");
+
+        m_equiped = true;
+
+        createData(1);  //index 0, 1 significa che l'oggetto da prendere è ancora inserito
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (m_doorTransitor.forward != m_equiped)
+            m_doorTransitor.forward = m_equiped;
+
+        base.Update();
+
+
+        if (m_inspectMode && m_equiped && !m_inventory.HasKey() && Input.GetKeyDown(KeyCode.E) && !m_camerasInTransition)
+        {
+
+            TakeKey();
+            UpdateUI();
+        }
+        else if (m_inspectMode && !m_equiped && m_inventory.HasRuby() && Input.GetKeyDown(KeyCode.E) && !m_camerasInTransition)
+        {
+
+            PlaceRuby();
+            UpdateUI();
+
+
+        }
+        else if (m_inspectMode && !m_equiped && m_inventory.HasKey() && Input.GetKeyDown(KeyCode.E) && !m_camerasInTransition)
+        {
+
+            PlaceKey();
+            UpdateUI();
+
+
+        }
+
+    }
+
+    private void TakeKey()
+    {
+
+        //inserito nell'inventario
+        m_inventory.PickKey();
+
+        //cancellato dalla scena
+
+        m_pickup1.SetActive(false);
+        m_equiped = false;
+
+        saveData(0, 0);   // salva che il primo oggetto è stato preso
+    }
+
+    private void PlaceKey()
+    {
+
+        //inserito nell'inventario
+        m_inventory.UseKey();
+
+        //cancellato dalla scena
+
+        m_pickup1.SetActive(true);
+        m_equiped = true;
+
+        saveData(0, 1);   // salva che il primo oggetto è stato tolto
+    }
+
+    private void PlaceRuby()
+    {
+
+        //inserito nell'inventario
+        m_inventory.UseRuby() ;
+
+        //cancellato dalla scena
+
+        m_pickup2.SetActive(true);
+        m_equiped = true;
+
+        saveData(0, 2);   // salva che il primo oggetto è stato tolto
+    }
+
+    protected override void UpdateUI()
+    {
+
+        // ***NON INSPECT MODE***
+         if (!m_inspectMode)
+        {
+           
+            m_actionText = "Premi E per Esaminare";
+            
+        }
+
+        base.UpdateUI();
+
+        // *** INSPECT MODE ***
+        if (!m_camerasInTransition)
+        {
+            if (m_inspectMode)
+            {
+                if (m_equiped && !m_inventory.HasKey())
+                    m_uiManager.ToggleActionPanel(true, "Premi E per Raccogliere la chiave");
+                else if(!m_equiped && m_inventory.HasKey() && !m_inventory.HasRuby())
+                    m_uiManager.ToggleActionPanel(true, "Premi E per mettere la chiave");
+                else if(!m_equiped && m_inventory.HasRuby())
+                    m_uiManager.ToggleActionPanel(true, "Premi E per mettere il rubino");
+                else
+                    m_uiManager.ToggleActionPanel(false);
+            }
+
+        }
+        
+      
+
+    }
+    
+
+    public override bool loadData(int t_key, ulong t_data)
+    {
+        bool find = base.loadData(t_key, t_data);
+        if (t_data == 0 && find)
+        {
+            m_equiped = false;
+            
+        }
+        return find;
+    }
+}
