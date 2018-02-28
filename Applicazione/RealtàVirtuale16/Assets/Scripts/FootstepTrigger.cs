f151a80b7874f6932fcfdf741d5102f81a59e562
Unknown <claudegia94@libero.it>
2018-02-28 11:20:47 +0100

    Test suono dei passi

---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/FootstepTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        AudioSource footstep = this.GetComponent<AudioSource>();
+        if (footstep != null)
+        {   if (!footstep.isPlaying)
+                footstep.PlayOneShot(footstep.clip);
+        }
+        else
+            Debug.LogError(this.name + ": Impossible to find audio source!");
+    }
+}
