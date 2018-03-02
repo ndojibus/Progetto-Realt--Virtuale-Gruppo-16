315cccff32d7235f2071dab13f8247266cf95c13
Italiankekko <33747967+Italiankekko@users.noreply.github.com>
2018-03-02 12:42:42 +0100

    Ultimo Commit

---
+++ Applicazione/RealtàVirtuale16/Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneControl.sceneControl.DeleteSave();
+            SceneControl.sceneControl.LoadNewScene("MainMenuScene");
+
+        }
+    }
+}
